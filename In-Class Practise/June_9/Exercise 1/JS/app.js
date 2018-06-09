b1cef4f9b9a56460f50af2a1810ef834d9d4dd8a
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2018-06-09 17:01:18 +0400

    Initial Commit

---
+++ In-Class Practise/June_9/Exercise 1/JS/app.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let admin, name;
+name = "John";
+admin = name;
+console.log(admin, name);
+alert(admin);
---
+++ In-Class Practise/June_9/JS/app.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const COLOR_GREEN = '#0fo';
+const COLOR_ORANGE = '#ff7f00';
+// debugger;
+// alert("Hello World!...");
+// confirm("Are you okay");
+// prompt("enter number:");
+
+// ASI
+
+alert("1") 
+alert("2")
+
+/*
+    multiline comment
+
+*/
+
+// var, let, const 
+
+let message;
+message = "my message";
+
+console.log(message);
+
+let user = 'John', age = 33;
+
+console.log(message, user, age); /* warn; info; */
+
+let _ladc;
+// camelCase
+
+// num = 13;
+// console.log(num);
+
+var num = 15;
+{
+    var num = 17;
+    // console.log(num);
+}
+
+console.log(num);
+
+// console.log("%c Hello Color", "backgorund-color:red; color: #fff; padding: 10px;");
+
+let foo = 99;
+console.log( typeof foo )
+foo = "bar";
+console.log( typeof foo )
+foo = true;
+console.log( typeof foo )
+foo = null;
+console.log( typeof foo )
+foo = undefined;
+console.log( typeof foo )
+console.log( "%c", "background-color:red; padding: 3px;");
+console.log( 1 / 0);
+console.log(Infinity);
+console.log("number"*2); // isNan()
+let number = 7;
+console.log(number+5*2);
+// null
+
+/* -*Python*-
+    $age = 13;
+    $message = "Sheni asakia: $age"; 
+*/
+
+age = 13;
+message = "Sheni asakia: " + age;
+console.log(message);
+message = 'Sheni asakia: ${age}';
+console.log(message);
+
+let name = "Dan";
+
+// alert('hello ${name + 1}'); // ?
+
+// alert('hello ${"name"}'); // ?
+
+// alert("hello ${name}"); // ?
+
+// alert('hello ${"name"}'); // ?
+
+// let checked = false;
+// let isValid = true;
+
+// let isGreater = 5 > 6;
+
+// age = parseInt(prompt("Romeli goda xar brat?"));
+// console.log(typeof age);
+
+// let isOkay = confirm("Xo kargad xar e?");
+// console.log(isOkay);
+
+// console.log(5 / 2);
+// console.log( parseInt(5 / 2));
+
+// console.log(5 % 2);
+// console.log( parseInt(8 % 3));
+
+
