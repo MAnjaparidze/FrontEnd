d2810cd67199cf034cfcc8de8e1940b1c8abcd8d
Mamuka Anjaparidze <anjaparidzemamuka@gmail.com>
2018-06-20 21:41:08 +0400

    Initial Commit

---
+++ In-Class Practise/June_20/Exercise 2/JS/main.js
@@ -0,0 +1,12 @@
+let nums = [];
+
+function sumInput() {
+    let sum = 0;
+
+    let sum = 0;
+    do{
+        n = Number(prompt("Please Enter a Number: "));
+        numbers.push(n);
+    }while
+
+}
--- In-Class Practise/June_20/JS/main.js
+++ In-Class Practise/June_20/JS/main.js
@@ -1,5 +1,97 @@
-function defaultParams(x = 13, y = 1) {
-    return x * y;
-}
+// function defaultParams(x = 13, y = 1) {
+//     return x * y;
+// }
 
-console.log(defaultParams(5));
+// console.log(defaultParams( x, 5));
+
+//#region Arrays
+
+// let years = new Array(10);
+// let years2 = [];
+
+// console.log(years.length);
+// years.length = 5;
+// console.log(years.length);
+
+// console.log("-------------");
+// console.log(years2.length);
+// years2.length = 17;
+// console.log(years2.length);
+
+//#endregion Arrays
+
+let fruits = ["Apple", "Banana", "Cherry"];
+
+// // fruits.length - 1
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// fruits[2] = "Lemon";
+// console.log(fruits);
+
+// let arr = ['Appe', { name:"John"}, true, () => { alert("Hello from array") }];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1].name);
+// console.log(arr[2]);
+// arr[3]();
+
+// let numbers = [];
+// // Push/Pop
+// numbers.push(1);
+// numbers.push(3);
+// numbers.push(8);
+// numbers.push(13);
+// console.log(numbers);
+// let removed = numbers.pop();
+// FIFO/LIFO
+// console.log('removed: ${removed}');
+// console.log(numbers);
+// let removed = numbers.pop();
+// // LIFO
+// console.log('removed: ${removed}');
+// console.log(numbers);
+// numbers.length = 0;
+// console.log(numbers);
+
+//FIFO
+//shift/unshift
+// numbers.unshift(1);
+// numbers.unshift(2);
+// numbers.unshift(8);
+// numbers.unshift(13);
+
+// console.log(numbers);
+// rmeoved = numbers.shift();
+// console.log('removed: #{removed}');
+// console.log(numbers);
+
+// let n = numbers;
+// console.log(n);
+// // console.log(n === numbers);
+// n.push(33);
+// console.log(numbers);
+// // let clone = Object.assign({}, numbers);
+
+// let clone = numbers.slice();
+// clone.push(64);
+// console.log(numbers)
+
+// CloseEvent.age = 11;
+// // console.log(clone);
+
+// // numbers[index]
+// for( let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+// for( number of numbers) {
+//     console.log(numbers);
+// }
+
+// let matrix = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9],
+// ]
+
