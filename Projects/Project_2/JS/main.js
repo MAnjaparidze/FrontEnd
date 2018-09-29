be7b09a31b87c06388ef7dd3e65b0c391ad97ba4
MAnjaparidze <MAnjaparidze@users.noreply.github.com>
2018-09-29 19:44:08 +0400

    I

--- Projects/Project_2/JS/main.js
+++ Projects/Project_2/JS/main.js
@@ -1,36 +1,93 @@
-let userInterface = document.getElementById("userInterface");
-let canvas = document.getElementById("playGround");
-let userName = document.getElementById("userName").value;
+var userInterface = document.getElementById("userInterface");
+var menu = document.getElementById("menu");
+var mainDisplay = document.getElementById("mainDisplay");
+var canvas = document.getElementById("playGround");
+var userName = document.getElementById("userName");
+
+var btnMenu = document.getElementById("menuBtn");
+var btnBack = document.getElementById("backBtn");
+var btnEasy = document.getElementById("easy");
+var btnMedium = document.getElementById("medium");
+var btnHard = document.getElementById("hard");
+var btnStart = document.getElementById("start");
+
+var btns = [btnEasy, btnMedium, btnHard];       // Creating array of numbers
 
 
-let btnEasy = document.getElementById("easy");
-let btnMedium = document.getElementById("medium");
-let btnHard = document.getElementById("hard");
-
-var btns = [btnEasy, btnMedium, btnHard];
-
-btns.forEach(function (button) {
-    button.addEventListener('click', function () {
-        startTheGame();
-
-    });
-});
-
-
-function startTheGame() {
-    $(userInterface).fadeOut();
-    setInterval(function () {
-        $(canvas).fadeIn();
-    }, 1000);
-}
+//#region Classes6
 
 class User {
     constructor(name, difficulty) {
         this.name = name;
         this.difficulty = difficulty;
     }
+}
+
+// class Snake {
+//     constructor(size) {
+//         this.size = size;
+//     }
+
+//     drawSnake(x, y) {
+//         var snakeSize = this.size;
+
+//         canvas.fillStyle = 'green';
+//         canvas.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+//         // This is the border of the square
+//         canvas.strokeStyle = 'darkgreen';
+//         canvas.strokeRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
+
+//         snake = [];
+
+//         for (var i = size; i >= 0; i--) {
+//             snake.push({ x: i, y: 0 });
+//         }
+//     }
+// }
+
+
+class Apples {
+    constructor(size) {
+        this.size = size;
+    }
+
+    randomAppear() {
+
+    }
 
 }
+
+//#endregion Classes
+
+btnMenu.addEventListener('click', function(){
+    $("#userInterface").fadeOut();
+    setInterval(function () {
+        $("#menu").fadeIn();
+    }, 1000);
+});
+
+btnBack.addEventListener('click', function(){
+    $("#menu").fadeOut();
+    setInterval(function () {
+        $("#userInterface").fadeIn();
+    }, 1000);
+})
+
+btnStart.addEventListener('click', function () { // Assigning Events of startTheGame function to Start Button
+    startTheGame();
+});
+
+
+//#region Functions
+
+function startTheGame() {       // Creating the function to fade the User interface and open the game
+    $(userInterface).fadeOut();
+    setInterval(function () {
+        $(canvas).fadeIn();
+    }, 1000);
+}
+
+//#endregion Functions
 
 
 
