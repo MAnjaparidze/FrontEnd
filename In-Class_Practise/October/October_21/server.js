a6d4346dca300850cb918b429b40258e82bb5b04
MAnjaparidze <MAnjaparidze@users.noreply.github.com>
2018-10-22 21:41:26 +0400

    October 21_2

--- In-Class_Practise/October/October_21/server.js
+++ In-Class_Practise/October/October_21/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path    = require('path');
+const countries = require('./countries');
 const app     = express();
 
 app.use(  express.static( path.join(__dirname, 'public') ) );
+app.use(  express.static( path.join(__dirname, '/node_modules/jquery') ) );
+
+
 app.use( express.urlencoded({ extended: true }));
 
 let PORT = process.env.PORT || 3000;
@@ -22,6 +26,17 @@ app.get('/add', (req, res) => {
     setTimeout( () => {
         res.send(result.toString());
     }, 2000)
+})
+
+app.get('/countries/:prefix', (req, res) => {
+    if( ! req.params.prefix ){
+        return res.sendStatus(400);
+    }
+    let prefix = req.params.prefix.toUpperCase();
+    let result = countries
+                    .filter(  country => country.name.toUpperCase().startsWith(prefix) )
+                    .map( country => country.name);
+    res.json(result);
 })
 
 app.listen(PORT, () => {
